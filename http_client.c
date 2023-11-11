b193d5671272243dcb54e869cfd1b08adf194487
Vlad Nikolic <vladislavdnikolic@gmail.com>
2023-11-10 19:06:02 -0500

    add missing files

---
+++ http_client.c
@@ -0,0 +1,385 @@
+/******************************************************************************
+* File Name:   http_client.c
+*
+* Description: This file contains task and functions related to HTTP client
+* operation.
+*
+*******************************************************************************
+* (c) 2019-2020, Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software ("EULA").
+*
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress's integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+*******************************************************************************/
+
+/* Header file includes. */
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+#include "cJSON.h"
+#include "GUI.h"
+#include "mtb_st7789v.h"
+#include "cy8ckit_028_tft_pins.h"
+#include <time.h>
+
+/* FreeRTOS header file. */
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+
+/* Standard C header file. */
+#include <string.h>
+
+/* Cypress secure socket header file. */
+#include "cy_secure_sockets.h"
+
+/* Wi-Fi connection manager header files. */
+#include "cy_wcm.h"
+#include "cy_wcm_error.h"
+
+/* TCP client task header file. */
+#include "http_client.h"
+
+/* HTTP Client Library*/
+#include "cy_http_client_api.h"
+
+/*******************************************************************************
+* Macros
+********************************************************************************/
+#define BUFFERSIZE							(2048 * 2)
+#define SENDRECEIVETIMEOUT					(10000)
+
+// Macros for GUI
+#define TFT_LEFT_ALIGNED				(0)
+#define TFT_PRICE						(100)
+#define TFT_PERCENT						(225)
+#define TFT_ROW_ONE						(0)
+#define TFT_ROW_TWO						(30)
+#define TFT_ROW_THREE					(50)
+#define TFT_ROW_FOUR					(70)
+#define TFT_ROW_FIVE					(90)
+
+/*******************************************************************************
+* Function Prototypes
+********************************************************************************/
+cy_rslt_t connect_to_wifi_ap(void);
+void disconnect_callback(void *arg);
+
+/*******************************************************************************
+* Global Variables
+********************************************************************************/
+volatile bool connected;
+
+/*******************************************************************************
+ * Function Name: http_client_task
+ *******************************************************************************
+ * Summary:
+ *  Task used to establish a secure connection to a remote TCP server, request
+ *  data from the server, then print the data to the tft dispaly.
+ *
+ * Parameters:
+ *  void *args : Task parameter defined during task creation (unused).
+ *
+ * Return:
+ *  void
+ *
+ *******************************************************************************/
+void http_client_task(void *arg){
+    cy_rslt_t result;
+
+    /* The pins above are defined by the CY8CKIT-028-TFT library. If the display is being used on different hardware the mappings will be different. */
+	const mtb_st7789v_pins_t tft_pins =
+	{
+		.db08 = CY8CKIT_028_TFT_PIN_DISPLAY_DB8,
+		.db09 = CY8CKIT_028_TFT_PIN_DISPLAY_DB9,
+		.db10 = CY8CKIT_028_TFT_PIN_DISPLAY_DB10,
+		.db11 = CY8CKIT_028_TFT_PIN_DISPLAY_DB11,
+		.db12 = CY8CKIT_028_TFT_PIN_DISPLAY_DB12,
+		.db13 = CY8CKIT_028_TFT_PIN_DISPLAY_DB13,
+		.db14 = CY8CKIT_028_TFT_PIN_DISPLAY_DB14,
+		.db15 = CY8CKIT_028_TFT_PIN_DISPLAY_DB15,
+		.nrd  = CY8CKIT_028_TFT_PIN_DISPLAY_NRD,
+		.nwr  = CY8CKIT_028_TFT_PIN_DISPLAY_NWR,
+		.dc   = CY8CKIT_028_TFT_PIN_DISPLAY_DC,
+		.rst  = CY8CKIT_028_TFT_PIN_DISPLAY_RST
+	};
+
+	/* Initialize the display */
+	mtb_st7789v_init8(&tft_pins);
+	GUI_Init();
+	GUI_SetBkColor(GUI_BLACK); // Background Color
+	GUI_SetColor(GUI_WHITE); // Text Color
+	GUI_SetFont(&GUI_Font32B_ASCII); // Font Size
+
+	// Connect to wifi - configuration settings are in http_client.h
+	result = connect_to_wifi_ap();
+	CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+	// Initialize http client lib
+    result = cy_http_client_init();
+    if(result != CY_RSLT_SUCCESS){
+    	printf("HTTP Client Library Initialization Failed!\n");
+    	CY_ASSERT(0);
+    }
+
+    // Initialize serverInfo and credentials structs
+    cy_awsport_server_info_t serverInfo;
+    cy_awsport_ssl_credentials_t credentials;
+    (void) memset(&credentials, 0, sizeof(credentials));
+	(void) memset(&serverInfo, 0, sizeof(serverInfo));
+
+    // Server Info - configuration settings are in http_client.h
+    serverInfo.host_name = SERVERHOSTNAME;
+    serverInfo.port = SERVERPORT;
+
+    // Set the credentials information
+    credentials.root_ca = SSL_ROOTCA_PEM;
+	credentials.root_ca_size = sizeof( SSL_ROOTCA_PEM );
+	credentials.root_ca_verify_mode = CY_AWS_ROOTCA_VERIFY_REQUIRED;
+	credentials.client_cert = SSL_CLIENTCERT_PEM;
+	credentials.client_cert_size = sizeof( SSL_CLIENTCERT_PEM );
+	credentials.private_key = SSL_CLIENTKEY_PEM;
+	credentials.private_key_size = sizeof( SSL_CLIENTKEY_PEM );
+
+    // Disconnection Callback
+    cy_http_disconnect_callback_t disconnectCallback = (void*)disconnect_callback;
+
+    // Client Handle
+    cy_http_client_t clientHandle;
+
+    // Create the HTTP Client
+    result = cy_http_client_create(&credentials, &serverInfo, disconnectCallback, NULL, &clientHandle);
+    if(result != CY_RSLT_SUCCESS){
+		printf("HTTP Client Creation Failed!\n");
+		CY_ASSERT(0);
+	}
+
+    // For time.h
+    setenv("TZ","EST5EDT", 1);
+
+    // Main program loop
+    while(1){
+		if(!connected){
+			// Connect to the HTTP Server
+			result = cy_http_client_connect(clientHandle, SENDRECEIVETIMEOUT, SENDRECEIVETIMEOUT);
+			if(result != CY_RSLT_SUCCESS){
+				printf("HTTP Client Connection Failed!\nResult: %lx\n", result);
+			}
+			else{
+				printf("\nConnected to HTTP Server Successfully\n\n");
+				connected = true;
+			}
+		}
+
+		// Create Request
+		uint8_t buffer[BUFFERSIZE];
+		cy_http_client_request_header_t request;
+		request.buffer = buffer;
+		request.buffer_len = BUFFERSIZE;
+		request.method = CY_HTTP_CLIENT_METHOD_GET;
+		request.range_start = -1;
+		request.range_end = -1;
+		request.resource_path = AMDSTOCKQUOTERESOURCE;
+
+		// Create Header
+		cy_http_client_header_t header;
+		header.field = "Host";
+		header.field_len = strlen("Host");
+		header.value = SERVERHOSTNAME;
+		header.value_len = strlen(SERVERHOSTNAME);
+		uint32_t num_header = 1;
+
+		result = cy_http_client_write_header(clientHandle, &request, &header, num_header);
+		if(result != CY_RSLT_SUCCESS){
+			printf("HTTP Client Header Write Failed!\n");
+			CY_ASSERT(0);
+		}
+
+		// Var to hold the servers responses
+		cy_http_client_response_t response;
+
+		// Send get request
+		if(connected){
+			printf("Sending HTTP Request\n");
+			result = cy_http_client_send(clientHandle, &request, NULL, 0, &response);
+			if(result != CY_RSLT_SUCCESS){
+				printf("HTTP Client Send Failed!\n");
+			}
+		}
+
+		// Print response message
+		printf("Response received from httpbin.org/html:\n");
+		for(int i = 2; i < response.body_len - 2; i++){
+			printf("%c", response.body[i]);
+		}
+		printf("\n\n");
+
+		// Parse the JSON received
+		cJSON *root = cJSON_ParseWithLength((const char*)response.body + 1, response.body_len - 2); // Read the JSON
+		cJSON *symbol = cJSON_GetObjectItem(root,"symbol"); // Ticker
+		cJSON *price = cJSON_GetObjectItem(root,"price"); // Current Price
+		cJSON *change = cJSON_GetObjectItem(root,"changesPercentage"); // % change over the day
+		cJSON *dayLow = cJSON_GetObjectItem(root,"dayLow"); // Todays low
+		cJSON *dayHigh = cJSON_GetObjectItem(root,"dayHigh"); // Todays high
+		cJSON *open = cJSON_GetObjectItem(root,"open"); // Todays open price
+		cJSON *previousClose = cJSON_GetObjectItem(root,"previousClose"); // Previous closing price
+		cJSON *timestamp = cJSON_GetObjectItem(root,"timestamp"); // Unix time stamp
+
+		// Disconnect from server
+		result = cy_http_client_disconnect(clientHandle);
+		if(result != CY_RSLT_SUCCESS){
+			printf("HTTP Client Disconnect Failed!\n");
+		}
+		else{
+			printf("Disconnected from HTTP Server\n");
+			connected = false;
+		}
+
+		// Display the stock info
+		char symbol_s[25];
+		GUI_SetFont(&GUI_Font32B_ASCII); // Font Size
+		sprintf(symbol_s, "%s", symbol->valuestring);
+		GUI_DispStringAt(symbol_s, TFT_LEFT_ALIGNED, TFT_ROW_ONE);
+
+		char price_s[25];
+		sprintf(price_s, "$%.2f", price->valuedouble);
+		GUI_DispStringAt(price_s, TFT_PRICE, TFT_ROW_ONE);
+
+		char change_s[25];
+		if(change->valuedouble >= 0){
+			GUI_SetColor(GUI_GREEN); // Text Color
+			sprintf(change_s, "+%.2f%%\n", change->valuedouble);
+			GUI_DispStringAt(change_s, TFT_PERCENT, TFT_ROW_ONE);
+			GUI_SetColor(GUI_WHITE); // Text Color
+		}else{
+			GUI_SetColor(GUI_RED); // Text Color
+			sprintf(change_s, "%.2f%%\n", change->valuedouble);
+			GUI_DispStringAt(change_s, TFT_PERCENT, TFT_ROW_ONE);
+			GUI_SetColor(GUI_WHITE); // Text Color
+		}
+
+		char open_close[25];
+		GUI_SetFont(&GUI_Font24B_ASCII); // Font Size
+		sprintf(open_close, "$%.2f / $%.2f", previousClose->valuedouble, open->valuedouble);
+		GUI_DispStringAt("PC/O", TFT_LEFT_ALIGNED, TFT_ROW_TWO);
+		GUI_DispStringAt(open_close, TFT_PRICE, TFT_ROW_TWO);
+
+		char day_low_high[25];
+		sprintf(day_low_high, "$%.2f / $%.2f", dayLow->valuedouble, dayHigh->valuedouble);
+		GUI_DispStringAt("DL/DH", TFT_LEFT_ALIGNED, TFT_ROW_THREE);
+		GUI_DispStringAt(day_low_high, TFT_PRICE, TFT_ROW_THREE);
+
+		time_t updateTime = (time_t)timestamp->valueint;
+		GUI_SetFont(&GUI_Font20B_ASCII); // Font Size
+		GUI_DispStringAt(ctime(&updateTime), TFT_LEFT_ALIGNED, TFT_ROW_FIVE);
+
+		// Wait 6 mins before re-connecting and querying again
+		vTaskDelay(pdMS_TO_TICKS(360000));
+    }
+
+}
+
+/*******************************************************************************
+ * Function Name: connect_to_wifi_ap()
+ *******************************************************************************
+ * Summary:
+ *  Connects to Wi-Fi AP using the user-configured credentials, retries up to a
+ *  configured number of times until the connection succeeds.
+ *
+ *******************************************************************************/
+cy_rslt_t connect_to_wifi_ap(void)
+{
+    cy_rslt_t result;
+
+    /* Variables used by Wi-Fi connection manager.*/
+    cy_wcm_connect_params_t wifi_conn_param;
+
+    cy_wcm_config_t wifi_config = { .interface = CY_WCM_INTERFACE_TYPE_STA };
+
+    cy_wcm_ip_address_t ip_address;
+
+     /* Initialize Wi-Fi connection manager. */
+    result = cy_wcm_init(&wifi_config);
+
+    if (result != CY_RSLT_SUCCESS)
+    {
+        printf("Wi-Fi Connection Manager initialization failed!\n");
+        return result;
+    }
+    printf("Wi-Fi Connection Manager initialized.\r\n");
+
+     /* Set the Wi-Fi SSID, password and security type. */
+    memset(&wifi_conn_param, 0, sizeof(cy_wcm_connect_params_t));
+    memcpy(wifi_conn_param.ap_credentials.SSID, WIFI_SSID, sizeof(WIFI_SSID));
+    memcpy(wifi_conn_param.ap_credentials.password, WIFI_PASSWORD, sizeof(WIFI_PASSWORD));
+    wifi_conn_param.ap_credentials.security = WIFI_SECURITY_TYPE;
+
+    /* Join the Wi-Fi AP. */
+    for(uint32_t conn_retries = 0; conn_retries < MAX_WIFI_CONN_RETRIES; conn_retries++ )
+    {
+        result = cy_wcm_connect_ap(&wifi_conn_param, &ip_address);
+
+        if(result == CY_RSLT_SUCCESS)
+        {
+            printf("Successfully connected to Wi-Fi network '%s'.\n",
+                                wifi_conn_param.ap_credentials.SSID);
+            printf("IP Address Assigned: %d.%d.%d.%d\n", (uint8)ip_address.ip.v4,
+                    (uint8)(ip_address.ip.v4 >> 8), (uint8)(ip_address.ip.v4 >> 16),
+                    (uint8)(ip_address.ip.v4 >> 24));
+            return result;
+        }
+
+        printf("Connection to Wi-Fi network failed with error code %d."
+               "Retrying in %d ms...\n", (int)result, WIFI_CONN_RETRY_INTERVAL_MSEC);
+
+        vTaskDelay(pdMS_TO_TICKS(WIFI_CONN_RETRY_INTERVAL_MSEC));
+    }
+
+    /* Stop retrying after maximum retry attempts. */
+    printf("Exceeded maximum Wi-Fi connection attempts\n");
+
+    return result;
+}
+
+/*******************************************************************************
+ * Function Name: disconnect_callback
+ *******************************************************************************
+ * Summary:
+ *  Invoked when the server disconnects
+ *
+ * Parameters:
+ *  void *arg : unused
+ *
+ * Return:
+ *  void
+ *
+ *******************************************************************************/
+void disconnect_callback(void *arg){
+    printf("Disconnected from HTTP Server\n");
+    connected = false;
+}
+
---
+++ http_client.h
@@ -0,0 +1,115 @@
+/******************************************************************************
+* File Name:   http_client.h
+*
+* Description: This file contains declaration of task related to HTTP client
+* operation.
+*
+*******************************************************************************
+* (c) 2019-2020, Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software ("EULA").
+*
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress's integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+*******************************************************************************/
+
+#ifndef HTTP_CLIENT_H_
+#define HTTP_CLIENT_H_
+
+/*******************************************************************************
+* Macros
+********************************************************************************/
+/* Wi-Fi Credentials: Modify WIFI_SSID, WIFI_PASSWORD and WIFI_SECURITY_TYPE
+ * to match your Wi-Fi network credentials.
+ * Note: Maximum length of the Wi-Fi SSID and password is set to
+ * CY_WCM_MAX_SSID_LEN and CY_WCM_MAX_PASSPHRASE_LEN as defined in cy_wcm.h file.
+ */
+
+#define WIFI_SSID							"SSID"
+#define WIFI_PASSWORD						"PSWD"
+
+#define SERVERHOSTNAME						"financialmodelingprep.com"
+#define SERVERPORT							(443)
+
+#define AMDSTOCKQUOTERESOURCE				"/api/v3/quote/AMD?apikey=<APIKEY>"
+
+/* Security type of the Wi-Fi access point. See 'cy_wcm_security_t' structure
+ * in "cy_wcm.h" for more details.
+ */
+#define WIFI_SECURITY_TYPE                 CY_WCM_SECURITY_WPA2_AES_PSK
+
+/* Maximum number of connection retries to the Wi-Fi network. */
+#define MAX_WIFI_CONN_RETRIES             (10u)
+
+/* Wi-Fi re-connection time interval in milliseconds */
+#define WIFI_CONN_RETRY_INTERVAL_MSEC     (1000)
+
+#define MAKE_IPV4_ADDRESS(a, b, c, d)     ((((uint32_t) d) << 24) | \
+                                          (((uint32_t) c) << 16) | \
+                                          (((uint32_t) b) << 8) |\
+                                          ((uint32_t) a))
+
+#define SSL_CLIENTCERT_PEM      \
+"-----BEGIN CERTIFICATE-----\n"\
+"-----END CERTIFICATE-----\n"
+
+
+/* SSL client private key. */
+#define SSL_CLIENTKEY_PEM          \
+"-----BEGIN PRIVATE KEY-----\n"\
+"-----END PRIVATE KEY-----\n"
+
+
+#define SSL_ROOTCA_PEM            \
+"-----BEGIN CERTIFICATE-----\n"\
+"MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\n"\
+"ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\n"\
+"b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\n"\
+"MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\n"\
+"b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\n"\
+"ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\n"\
+"9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\n"\
+"IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\n"\
+"VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\n"\
+"93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\n"\
+"jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\n"\
+"AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\n"\
+"A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\n"\
+"U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\n"\
+"N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\n"\
+"o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\n"\
+"5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\n"\
+"rqXRfboQnoZsG4q5WTP468SQvvG5\n"\
+"-----END CERTIFICATE-----\n"
+
+
+
+/*******************************************************************************
+* Function Prototypes
+********************************************************************************/
+void http_client_task(void *arg);
+
+#endif /* HTTP_CLIENT_H_ */
